9ff35b4aff933d9513ae65d1d78315beee722f9a
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-02 11:40:51 -0300

    feat: successful API search

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { useState, useEffect } from "react";
 import ShowStudents from "./components/ShowStudents";
 
 function App() {
+  const [students, setStudents] = useState([]);
+  console.log(students);
+  useEffect(() => {
+    fetch("https://hp-api.herokuapp.com/api/characters/students")
+      .then((response) => response.json())
+      .then((response) => setStudents(response));
+  }, []);
+
   return <div className="App"></div>;
 }
 
