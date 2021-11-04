cd1b55ef170b6d5e12c9a563af089c6414a8b6dd
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-04 08:51:51 -0300

    implements home page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ShowStudents from "./components/ShowStudents";
 
 function App() {
   const [students, setStudents] = useState([]);
+  const [home, setHome] = useState(true);
+
   useEffect(() => {
     fetch("https://hp-api.herokuapp.com/api/characters/students")
       .then((response) => response.json())
@@ -14,7 +16,33 @@ function App() {
       );
   }, []);
 
-  return (
+  return home ? (
+    <div className="box_home">
+      <div className="box_home__title">
+        <p>TORNEIO</p>
+        <p>TRIBRUXO</p>
+      </div>
+      <div className="box_home__button">
+        <button onClick={() => setHome(!home)}>
+          <i class="fas fa-quidditch"></i>
+        </button>
+      </div>
+      <div className="box_home__houses">
+        <div className="homeG__faixa">
+          <div className="homeG"></div>
+        </div>
+        <div className="homeS__faixa">
+          <div className="homeS"></div>
+        </div>
+        <div className="homeR__faixa">
+          <div className="homeR"></div>
+        </div>
+        <div className="homeH__faixa">
+          <div className="homeH"></div>
+        </div>
+      </div>
+    </div>
+  ) : (
     students.length > 0 && (
       <div className="App">
         <ShowStudents students={students} />
